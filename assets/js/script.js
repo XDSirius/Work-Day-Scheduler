47cfe8770d24c80da996a752b9e5fcd8791a917a
Yasmin Castillo <yasmin_c@outlook.com>
2023-06-05 15:24:07 -0500

    made a function to distinguished if the block hour is past present or future depending on the current hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,10 +21,24 @@ $(function () {
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
     //
+    
+    $(".time-block").each(function(){
+      var blockHour = parseInt($(this).attr("id").split("-")[1]);
+      var currentHour = dayjs().hour();
+      if (blockHour < currentHour){
+        $(this).addClass("past");
+      }else if (blockHour == currentHour){
+        $(this).addClass("present");
+      }else{
+        $(this).addClass("future");
+      }
+    })
+
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
     //
+    
     // TODO: Add code to display the current date in the header of the page.
     $("#currentDay").text(dayjs().format("dddd MMMM D YYYY"));
   });
